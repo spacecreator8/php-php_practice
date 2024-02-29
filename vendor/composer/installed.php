785c0ef0892c486a93b7330fe4f72bb79fee9206
spacecreator8 <spcc888@ya.ru>
2024-02-29 09:52:32 +0700

    Изменил мелочи

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'spacecreator/php_practice',
+        'reference' => '8cb64e3cc28c12c9240e323037a87773e8884d9f',
+        'name' => 'spacecreator/php-php_practice',
         'dev' => true,
     ),
     'versions' => array(
-        'spacecreator/php_practice' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+        'spacecreator/php-php_practice' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '8cb64e3cc28c12c9240e323037a87773e8884d9f',
             'dev_requirement' => false,
         ),
     ),
