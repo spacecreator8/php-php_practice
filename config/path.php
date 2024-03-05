6427d1880560b55bb2098334ac3411bf18880a4b
spacecreator8 <spcc888@ya.ru>
2024-03-05 11:27:04 +0700

    На всякий случай

--- config/path.php
+++ config/path.php
@@ -1,10 +1,6 @@
 <?php
 return [
     'root'=>'php_practice',
-    // 'classes'=>[
-    //     'app',
-    //     'core'
-    // ],
     'routes'=>'routes',
     'views'=>'views'
 ];
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,23 +1,8 @@
 <?php
 
-
-
 //Путь до директории с конфигурационными файлами
 const DIR_CONFIG = '/../config';
 require_once  __DIR__ . '/../vendor/autoload.php';
-
-// //Добавляем пользовательскую функцию автозагрузки классов
-// spl_autoload_register(function ($className) {
-//    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
-//    $className = str_replace('\\', '/', $className);
-
-//    foreach ($paths['classes'] as $path) {
-//        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
-//        if (file_exists($fileName)) {
-//            require_once $fileName;
-//        }
-//    }
-// });
 
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
